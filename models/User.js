6474be9fb6d23f2560b7afbb1c07ccb15f4d4a36
Dalglish Fernandes <dalglishfernandesuk@gmail.com>
2020-12-28 21:08:39 +0000

    2nd day dev

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail} = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -19,6 +20,21 @@ const userSchema = new mongoose.Schema({
         trim: true,
         validate: [() => {}, "Enter a Valid Password"]
     }
+})
+
+// Fire a function after Document is saved in Mongo DB
+// userSchema.post('save', async (doc, next) => {
+//     console.log("The data is added", doc);
+//     next();
+// })
+
+// Fire a function before Document is saved in Mongo DB
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    console.log("user about to be created and saved", this);
+
+    next();
 })
 
 const User = mongoose.model('user', userSchema);
